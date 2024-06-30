570d16bb368c918b7eb84a3de02f99d7e5a30daa
tooooowa <towa_0111@softbank.ne.jp>
2024-06-30 16:26:59 +0900

    環境構築

---
+++ src/main/kotlin/com/github/tooooowa/simplelocalchat/SimpleLocalChat.kt
@@ -0,0 +1,13 @@
+package com.github.tooooowa.simplelocalchat
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimpleLocalChat : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
